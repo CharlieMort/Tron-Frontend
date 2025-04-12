8043f66b9203be278b3c62492b921aedf42bc495
CharlieMort <charlie_mort@outlook.com>
2025-04-12 18:43:37 +0100

    test

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
 func main() {
+	fmt.Println("Welcome...")
 	http.Handle("/", http.FileServer(http.Dir("./src")))
 	http.ListenAndServe(":3000", nil)
 }
