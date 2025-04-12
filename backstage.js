bc2e4cff3a3612ea5481e08a9f2de14c633e09cf
CharlieMort <charlie_mort@outlook.com>
2025-04-12 17:05:31 +0100

    Added Title Screen

--- backstage.js
+++ backstage.js
@@ -1,10 +1,13 @@
 const fps = 30;
-const intro_delay = 0.5;
+const intro_delay = 6500;
 let c = undefined;
 let canvas = undefined;
 const gridSize = 10;
 const gridWidth = 500;
 const gridHeight = 800
+let mouseX = 0
+let mouseY = 0
+let clicked = false
 
 setTimeout(() => {
     c = document.getElementById("canvas");
@@ -16,10 +19,72 @@ setTimeout(() => {
     }, 1000/fps)
 }, intro_delay)
 
+function mouseMoveHandler(e) {
+    var rect = c.getBoundingClientRect();
+    mouseX = e.clientX - rect.left
+    mouseY = e.clientY - rect.top
+  }
+
+document.addEventListener("mousemove", mouseMoveHandler, false);
+document.addEventListener("mousedown", (event) => {
+    clicked = true;
+    console.log("mousedown");
+});
+document.addEventListener("mouseup", (event) => {
+    clicked = false
+    console.log("mouseup")
+});
+
 function drawRect(x, y, w, h, c) {
+    canvas.shadowColor = c
+    canvas.shadowBlur = gridSize/2
+    canvas.shadowOffsetX = -2
+    canvas.fillStyle = c
+    canvas.fillRect(x, y, w, h);
+
+    canvas.shadowColor = c
+    canvas.shadowBlur = gridSize/2
+    canvas.shadowOffsetX = 2
     canvas.fillStyle = c
     canvas.fillRect(x, y, w, h);
 }
+
+function drawStrokeRect(x, y, w, h, c) {
+    canvas.shadowColor = c
+    canvas.shadowBlur = gridSize/2
+    canvas.shadowOffsetX = -2
+    canvas.strokeStyle = c
+    canvas.lineWidth = 3
+    canvas.strokeRect(x, y, w, h);
+
+    canvas.shadowOffsetX = 2
+    canvas.strokeRect(x, y, w, h);
+
+    canvas.shadowOffsetY = 2
+    canvas.strokeRect(x, y, w, h);
+    canvas.shadowOffsetY = -2
+    canvas.strokeRect(x, y, w, h);
+}
+
+function drawText(txt, x, y, s, c, f="Arial", sc) {
+    canvas.font = `${s}px ${f}`;
+    canvas.textAlign = "center"
+    canvas.fillStyle = c
+    if (sc) {
+        canvas.shadowColor = c
+        canvas.shadowBlur = gridSize/2
+        canvas.shadowOffsetX = -5
+        canvas.shadowOffsetY = -5
+    } else {
+        canvas.shadowBlur = 0
+    }
+    canvas.fillText(`${txt}`, x, y)
+}
+
+function drawButton(txt, x, y, w, h, ts, sc, tc, f="Arial") {
+    drawStrokeRect(x, y, w, h, sc)
+    drawText(txt, x+w/2, y+h/1.75, ts, tc, f)
+} 
 
 class GameObject {
     constructor(width, height, x, y, color) {
@@ -28,6 +93,46 @@ class GameObject {
         this.x = x;
         this.y = y;
         this.color = color
+    }
+}
+
+class Button {
+    constructor(txt, x, y, w, h, ts, sc, tc, f, och) {
+        this.txt = txt;
+        this.x = x
+        this.y = y
+        this.width = w
+        this.height = h
+        this.textSize = ts
+        this.strokeColor = sc
+        this.textColor = tc
+        this.font = f
+        this.hoverColor = "#ffa500"
+        this.standardColor = this.strokeColor
+        this.onClickHandler = och
+    }
+
+    Draw() {
+        drawButton(this.txt, this.x, this.y, this.width, this.height, this.textSize, this.strokeColor, this.textColor, this.font)
+    }
+
+    Update() {
+        if (mouseX >= this.x && mouseX <= this.x+this.width && mouseY >= this.y && mouseY <= this.y+this.height) {
+            this.textColor = this.hoverColor
+            this.strokeColor = this.hoverColor
+            if (clicked) {
+                this.OnClick()
+            }
+        } else {
+            this.textColor = this.standardColor
+            this.strokeColor = this.standardColor
+        }
+        this.Draw()
+    }
+
+    OnClick() {
+        clicked = false
+        this.onClickHandler()
     }
 }
 
@@ -44,12 +149,23 @@ class Rect extends GameObject {
 class Bike extends GameObject {
     constructor(x=200, y=300, color="#00ffff", id) {
         super(gridSize, gridSize, x, y, color)
+        this.startX = x
+        this.startY = y
         this.body = new Rect(this.width, this.height, this.x, this.y, this.color);
         this.direction = 1
         this.speed = 5 // has to be a factor of grid size - 20
         this.turnQ = []
         this.id = id
         this.alive = true
+    }
+
+    reset() {
+        this.alive = true;
+        this.x = this.startX
+        this.y = this.startY
+        this.turnQ = []
+        this.direction = 1
+        console.log("reset")
     }
 
     signalRight() {
@@ -73,7 +189,7 @@ class Bike extends GameObject {
     }
 
     Update() {
-        if (this.alive) {
+        if (this.alive && running) {
             switch(this.direction) {
                 case 1:
                     this.y -= this.speed;
@@ -88,6 +204,10 @@ class Bike extends GameObject {
                     this.x -= this.speed;
                     break;
             }
+        }
+
+        if (this.x < 0 || this.y < 0) {
+            this.alive = false;
         }
         
         if (this.x%gridSize == 0 && this.y%gridSize == 0) {
--- main.js
+++ main.js
@@ -1,8 +1,44 @@
 let player;
 let player2;
 let grid;
+let score = 0;
+let score2 = 0;
+let running = true;
+let countdown = 0;
+let gamestate = "titleScreen"
+
+let localBut;
+let onlineBut;
 
 function Start() {
+    StartTitle()
+}
+
+function Update() {
+    switch(gamestate) {
+        case "titleScreen":
+            TitleLoop()
+            break;
+        case "gaming":
+            GameLoop()
+            break;
+    }
+}
+
+function StartTitle() {
+    localBut = new Button("Local", 25, 500, 200, 100, 35, "#0ff", "#0ff", "Orbitron", () => {
+        StartGame()
+    })
+    onlineBut = new Button("Online", 275, 500, 200, 100, 35, "#0ff", "#0ff", "Orbitron", () => {console.log("online")})
+}
+
+function TitleLoop() {
+    drawText("Tron", gridWidth/2, gridHeight/2, 150, "#00ffff", "Orbitron", "#00cccc")
+    localBut.Update()
+    onlineBut.Update()
+}
+
+function StartGame() {
     player = new Bike(100, 600)
     player2 = new Bike(400, 600, "#FFA500", 1)
     document.addEventListener('keypress', function(event) {
@@ -23,19 +59,40 @@ function Start() {
         }
     });
 
-    grid = [];
-    for (let i = 0; i<gridHeight/gridSize; i++) {
-        let tmp = []
-        for (let j = 0; j<gridWidth/gridSize; j++) {
-            tmp.push("");
-        }
-        grid.push(tmp)
-    }
+    resetGrid()
+    gamestate = "gaming"
 }
 
-function Update() {
+function GameLoop() {
     player.Update()
     player2.Update()
+
+    if (running) {
+        if (player.alive && !player2.alive) {
+            score += 1
+            nextRound()
+        }
+        if (player2.alive && !player.alive) {
+            score2 += 1
+            nextRound()
+        }
+        if (!player2.alive && !player.alive) {
+            nextRound()
+        }
+    } else if (countdown >= 0.1) {
+        countdown -= 1/fps
+        canvas.font = "100px Arial";
+        canvas.textAlign = "center"
+        canvas.fillStyle = "#00ffff"
+        canvas.fillText(`${Math.ceil(countdown)}`, gridWidth/2, gridHeight/2-100)
+    } else {
+        resetGrid()
+        resetPlayers()
+        running = true
+    }
+
+    drawText(`${score} - ${score2}`, gridWidth/2, gridHeight/2, 100, "#ffa500")
+
     grid.map((row, r) => {
         row.map((ele, c) => {
             if (ele == `${player.id}`) {
@@ -45,4 +102,25 @@ function Update() {
             }
         })
     })
+}
+
+function nextRound() {
+    countdown = 2
+    running = false
+}
+
+function resetPlayers() {
+    player.reset()
+    player2.reset()
+}
+
+function resetGrid() {
+    grid = [];
+    for (let i = 0; i<gridHeight/gridSize; i++) {
+        let tmp = []
+        for (let j = 0; j<gridWidth/gridSize; j++) {
+            tmp.push("");
+        }
+        grid.push(tmp)
+    }
 }
