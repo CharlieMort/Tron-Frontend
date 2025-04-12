a5b8fd453b3ce3af496c78dc3e98fa82efa8662a
CharlieMort <charlie_mort@outlook.com>
2025-04-12 19:40:48 +0100

    asd

--- main.go
+++ main.go
@@ -3,10 +3,31 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func main() {
-	fmt.Println("Welcome...")
-	http.Handle("/", http.FileServer(http.Dir("/app/src")))
-	http.ListenAndServe(":3000", nil)
+	// Replace "." with the actual path of the directory you want to expose.
+	directoryPath := "./src"
+
+	// Check if the directory exists
+	_, err := os.Stat(directoryPath)
+	if os.IsNotExist(err) {
+		fmt.Printf("Directory '%s' not found.\n", directoryPath)
+		return
+	}
+
+	// Create a file server handler to serve the directory's contents
+	fileServer := http.FileServer(http.Dir(directoryPath))
+
+	// Create a new HTTP server and handle requests
+	http.Handle("/", fileServer)
+
+	// Start the server on port 8080
+	port := 3000
+	fmt.Printf("Server started at http://localhost:%d\n", port)
+	err = http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	if err != nil {
+		fmt.Printf("Error starting server: %s\n", err)
+	}
 }
